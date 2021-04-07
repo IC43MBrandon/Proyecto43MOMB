d1564788f1e46e4453b1005d6d5c62ef015817ba
IC43MBrandon <81886608+IC43MBrandon@users.noreply.github.com>
2021-04-07 18:26:48 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyBLJkF0B1dakrZA0cYVSY37R0mVsq2ov9Q",
+    authDomain: "brandon-8ed1d.firebaseapp.com",
+    projectId: "brandon-8ed1d",
+    storageBucket: "brandon-8ed1d.appspot.com",
+    messagingSenderId: "586411405339",
+    appId: "1:586411405339:web:1a2e4db6ae6b95b73f48be",
+    measurementId: "G-6T1J5ERLB7"
+  
+  });
